bf496dd3eea5af081b1724f50de2bd1383d97d3b
KaydenGiang2512 <nhatkhanhgiang2512@gmail.com>
2022-07-22 10:55:44 +0700

    Initial Commit

---
+++ KnowYourGreatest/KnowYourGreatestApp.swift
@@ -0,0 +1,17 @@
+//
+//  KnowYourGreatestApp.swift
+//  KnowYourGreatest
+//
+//  Created by Khanh Giang Nhat on 22/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct KnowYourGreatestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
