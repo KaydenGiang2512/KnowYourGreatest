18966b5c46722d4e059062c0d17ed718b63908eb
KaydenGiang2512 <nhatkhanhgiang2512@gmail.com>
2022-08-03 11:38:14 +0700

    added JSON data file

--- KnowYourGreatest/Models/PlayerData.swift
+++ KnowYourGreatest/Models/PlayerData.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 var players: [Player] = [
-    Player(fullName: "Cristiano Ronaldo", age: "37", dateOfBirth: "Feb 5, 1985", jerseyNumbers: "7/9/28 (Clubs); 7/10/11/17 (National Team)", clubs: "Sporting CP (2002-2003); Manchester United (2003-2009, 2021-now); Real Madrid (2009-2018); Juventus (2018-2021)", numberOfAppearances: "1122", numberOfGoals: "815", numberOfAssists: "230", collectiveHonors: "1x Supertaça Cândido de Oliveiral; 3x Premier League; 1x FA Cup; 2x Football League Cup; 2x FA Community Shield; 2x La Liga; 2x Copa Del Rey; 2x Supercopa de Espana; 3x UEFA Super Cup; 4x Fifa Club World Cup; 2x Serie A; 1x Coppa Italia; 2x Supercoppa Italiana; 5x UEFA Champions League; 1x UEFA European Championship; 1x UEFA Nations League", individualHonors: "5x Ballon D'ors; 4x European Golden Shoe; 10x Portugal's Player of The Year; 1x UEFA Club Footballer of the Year; 2x UEFA Best Player in Europe; 1x UEFA Men's Player of the Year; 1x Puskas Award")
+    Player(fullName: "Cristiano Ronaldo", age: "37", dateOfBirth: "Feb 5, 1985", jerseyNumbers: "7/9/28 (Clubs); 7/10/11/17 (National Team)", clubs: "Sporting CP (2002-2003); Manchester United (2003-2009, 2021-now); Real Madrid (2009-2018); Juventus (2018-2021)", numberOfAppearances: "1122", numberOfGoals: "815", numberOfAssists: "230", )
 
 
 
