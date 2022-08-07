a8512d53edeb5d099d2c3bec4f295b2fb658ece5
Giang Nhật Khánh (Kayden) <73809007+KaydenGiang2512@users.noreply.github.com>
2022-08-07 09:50:55 +0700

    added header section

--- KnowYourGreatest/KnowYourGreatestApp.swift
+++ KnowYourGreatest/KnowYourGreatestApp.swift
@@ -5,6 +5,18 @@
 //  Created by Khanh Giang Nhat on 22/07/2022.
 //
 
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2022B
+  Assessment: Assignment 1
+  Author: Giang Nhat Khanh
+  ID: s3878182
+  Created  date: 25/07/2022
+  Last modified: 07/08/2022
+  Acknowledgement: Dr. Tom Huynh Lecture Slides, Apple Forums, YouTube, etc.
+*/
+
 import SwiftUI
 
 @main
